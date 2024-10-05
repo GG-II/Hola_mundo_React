30e8ed5ed5d38dd62adaf85ba1880dd2cdb9a9a3
GG-II <gdgl1105@gmail.com>
2024-10-05 08:49:46 -0600

    Adición de imagen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import miImagen from './images/hola.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 		<h1 style={{ color: 'blue', fontSize: '4rem' }}>¡Hola Mundo en React!</h1>
+		
+		<img src={miImagen} alt="Descripción de la imagen" className="mi-imagen" />
 	</div>
 
   );
